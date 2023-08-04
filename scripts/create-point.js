b127cd7a137a0b03e843d38e74ace7dc51977b5d
luizfspintoo <luizfspintoo@gmail.com>
2023-08-04 11:06:05 -0300

    create modal and page results

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,5 +1,5 @@
 function populateUFs() {
-    const ufSelect = document.querySelector("select[name='uf']");
+    const ufSelect = document.querySelector("select[name='uf']")
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
         .then((res) => {
             return res.json();
@@ -19,11 +19,14 @@ function getCitys(event) {
     const stateInput = document.querySelector("input[name='state']");
 
     const ufValue = event.target.value;
-    
+
     const indexOfSelectedState = event.target.selectedIndex;
     stateInput.value = event.target.options[indexOfSelectedState].text;
-    
+
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
+
+    citySelect.innerHTML += `<option value=''>Selecione a cidade</option>`;
+    citySelect.disabled = true;
 
     fetch(url)
         .then((res) => {
@@ -32,7 +35,7 @@ function getCitys(event) {
         .then(citys => {
 
             for (const city of citys) {
-                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
             }
 
             citySelect.disabled = false;
@@ -40,3 +43,40 @@ function getCitys(event) {
 }
 
 document.querySelector("select[name='uf']").addEventListener("change", getCitys);
+
+
+const itensToColeted = document.querySelectorAll(".itens-grid li");
+
+for (const item of itensToColeted) {
+    item.addEventListener("click", handleSelectedItens)
+}
+
+const colectedItens = document.querySelector("input[name='itens']");
+let itensSelected = [];
+
+function handleSelectedItens(event) {
+
+    const itemLi = event.target;
+
+    itemLi.classList.toggle("selected");
+
+    const itensId = itemLi.dataset.id;
+
+    const alreadySelected = itensSelected.findIndex(item => {
+        const itemFound = item == itensId
+        return itemFound;
+    })
+
+    if(alreadySelected >= 0) {
+        const filterAdd = itensSelected.filter(item => {
+            const itemIsDiference = item != itensId
+            return itemIsDiference;
+        })
+
+        itensSelected = filterAdd;
+    } else {
+        itensSelected.push(itensId);
+    }
+    colectedItens.value = itensSelected;
+}
+
