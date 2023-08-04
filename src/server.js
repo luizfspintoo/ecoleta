182092a960728a77c9e8473142ee1d245409d8ae
luizfspintoo <luizfspintoo@gmail.com>
2023-08-04 15:10:14 -0300

    working with nunjunks

--- src/server.js
+++ src/server.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const server = express();
 
+//diretório public disponivel
+server.use(express.static("public"))
+
+//configurar template engine, para trabalhar com lógicas no HTML
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
 //caminhos da minha aplicação (rotas)
 //req: Requisição
 //res: Resposta
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html");
+    return res.render("index.html", {title: "Um título"});
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html");
+})
+
+server.get("/search-results", (req, res) => {
+    return res.render("search-results.html");
 })
 
 
